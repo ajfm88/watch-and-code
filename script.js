e5244964175d4d3a072ba1f6fe72df1597485bf4
ajfm88 <ale@ajfm88.com>
2021-09-15 10:59:15 -0700

    Sept. 15, 2021

---
+++ script.js
@@ -0,0 +1,15 @@
+
+//Declaration       //Parameter
+function demoFunction(myData) {
+
+  console.log(myData);
+}
+
+//Assignment
+demoFunction('gordon');
+            //Argument
+
+
+//Declaration
+var myData = 'gordon'; //Assignment
+ //Variable  //Value
