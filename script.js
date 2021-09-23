39469e2aa499d5283320a2c4cd9bcdb8338a16f9
ajfm88 <ale@ajfm88.com>
2021-09-23 10:59:05 -0700

    Sept. 23rd, 2021

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ demoFunction('gordon');
 //Declaration
 var myData = 'gordon'; //Assignment
  //Variable  //Value
+
+ var gordon = {                   //object
+  name: 'Gordon',                 //property
+  city: 'San Francisco',          //property
+  myMethod: function sayHi() {  //method
+  console.log('Hi!');
+  }
+};
+
+//If a property happens to be a function, it's called a method
+
+gordon.myMethod(); //function call
+
+console.log('Hello World!') // log is a method on the console object
+
+// If you have data on an object, it's called a property
